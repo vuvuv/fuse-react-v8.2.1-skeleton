32d80756982cc616d62d4dc4c603e5d77443d570
Privatik3 <lol11999333@gmail.com>
2022-09-29 20:26:23 +0300

    Initial commit

---
+++ src/@mock-api/index.js
@@ -0,0 +1,13 @@
+import './api/dashboards/analytics-api';
+import './api/auth-api';
+import './api/notifications-api';
+import history from '@history';
+import mock from './mock';
+
+mock.onAny().passThrough();
+
+if (module?.hot?.status() === 'apply') {
+  const { pathname } = history.location;
+  history.push('/loading');
+  history.push({ pathname });
+}
